ee7f633ab59fb8aafdbe9d8f6db7c230724b2c9c
LeoTan2004 <3509836544@qq.com>
2024-11-06 19:42:42 +0800

    feat(service): 添加公共信息的刷新
    - 公共信息的刷新不依赖请求人的账号SESSION
    - 调整个人信息和公共信息服务的继承关系
    
    Signed-off-by: LeoTan2004 <3509836544@qq.com>

--- src/plat/service.py
+++ src/plat/service.py
@@ -6,7 +6,7 @@ from typing import TypeVar, Generic
 from plat.entity import TaskEntity
 from plat.repository.d_basic import KVRepository, SimpleKVRepository
 from plat.repository.d_cache import CacheRepository
-from plat.task import PersonalUpdateTask
+from plat.task import UpdateTask, PersonalUpdateTask
 from plat.validator import TaskValidator
 from xtu_ems.ems.handler import Handler
 
@@ -20,10 +20,44 @@ class IService(Generic[D]):
 
 
 class PersonalInfoService(IService[D]):
+    """个人信息服务"""
 
-    async def get_info(self, student_id: str) -> D | None:
-        task: TaskEntity = await self.storage.async_get_item(student_id)
+    async def get_info(self, key: str) -> D | None:
+        task: TaskEntity = await self.storage.async_get_item(key)
         return task.data
+
+    def generate_task(self, key: str,
+                      storage: KVRepository[str, TaskEntity]):
+        """
+
+        Args:
+            key: 更新内容的key
+            storage: 本地存储
+
+        Returns:
+            返回一个更新任务
+        """
+        return PersonalUpdateTask(key, self.handler, storage, self.account_repository)
+
+    def get_refresher(self):
+        async def on_refresh(key: str, value: TaskEntity, repo: KVRepository[str, TaskEntity]):
+            task = self.generate_task(key=key, storage=repo)
+            if not value:
+                value = TaskEntity()
+            value.on_submit_task()
+            task = asyncio.create_task(task())
+            await repo.async_set_item(key, value)
+            self.background_tasks.add(task)
+            task.add_done_callback(self.background_tasks.discard)
+            return value
+
+        return on_refresh
+
+    def get_updater(self):
+        async def on_update(key, value):
+            pass
+
+        return on_update
 
     def __init__(self, handler: Handler,
                  update_expire: timedelta,
@@ -34,24 +68,39 @@ class PersonalInfoService(IService[D]):
         self.handler = handler
         self.account_repository = account_repository
         self.background_tasks = set()
-
-        async def on_update(key, value):
-            pass
-
-        async def on_refresh(key: str, value: TaskEntity, repo: KVRepository[str, TaskEntity]):
-            session = await self.account_repository.async_get_item(key)
-            if session:
-                personal_task = PersonalUpdateTask(key, self.handler, repo, self.account_repository)
-                if not value:
-                    value = TaskEntity()
-                value.on_submit_task()
-                task = asyncio.create_task(personal_task())
-                await repo.async_set_item(key, value)
-                self.background_tasks.add(task)
-                task.add_done_callback(self.background_tasks.discard)
-            return value
-
         self.storage: [str, TaskEntity] = CacheRepository(local_cache=SimpleKVRepository[str, TaskEntity](),
                                                           validator=self.validator,
-                                                          on_write_back=on_update,
-                                                          on_refresh=on_refresh)
+                                                          on_write_back=self.get_updater(),
+                                                          on_refresh=self.get_refresher())
+
+
+class PublicInfoService(PersonalInfoService[D]):
+    """公共信息服务"""
+
+    async def get_info(self, student_id: str = None) -> D | None:
+        task: TaskEntity = await self.storage.async_get_item(self.name)
+        return task.data
+
+    async def get_public_info(self) -> D | None:
+        return await self.get_info()
+
+    def generate_task(self, key: str,
+                      storage: KVRepository[str, TaskEntity]):
+        """
+
+        Args:
+            key: 更新内容的key
+            storage: 本地存储
+
+        Returns:
+            返回一个更新任务
+        """
+        return UpdateTask(key, self.handler, storage, self.account_repository)
+
+    def __init__(self, name: str,
+                 handler: Handler,
+                 update_expire: timedelta,
+                 submit_expire: timedelta,
+                 account_repository: KVRepository):
+        super().__init__(handler, update_expire, submit_expire, account_repository)
+        self.name = name
--- tests/test_service.py
+++ tests/test_service.py
@@ -5,8 +5,9 @@ from unittest.mock import patch
 
 from plat.entity import Account, AccountStatus
 from plat.repository.d_basic import SimpleKVRepository
-from plat.service import PersonalInfoService
+from plat.service import PersonalInfoService, PublicInfoService
 from xtu_ems.ems.handler.get_student_courses import StudentCourseGetter
+from xtu_ems.ems.handler.get_teaching_calendar import TeachingCalendarGetter
 from xtu_ems.ems.handler.valid_session import SessionValidator
 
 session_repository = SimpleKVRepository()
@@ -37,7 +38,7 @@ class TestPersonalInfoService(IsolatedAsyncioTestCase):
     async def test_get_info_with_expired(self):
         """测试获取个人信息，但是老数据过期"""
         with patch.object(StudentCourseGetter, 'async_handler', return_value="Mocked Data"):
-            with patch.object(SessionValidator, 'async_handler', return_value=False):
+            with patch.object(SessionValidator, 'async_handler', return_value=True):
                 handler = StudentCourseGetter()
                 service = PersonalInfoService(handler=handler, update_expire=timedelta(microseconds=1),
                                               submit_expire=timedelta(microseconds=1),
@@ -47,6 +48,44 @@ class TestPersonalInfoService(IsolatedAsyncioTestCase):
                 # 如果数据没有更新，需要等待
                 await asyncio.sleep(.1)
                 result = await service.get_info(acc.student_id)
+                self.assertIsNotNone(result)
+                await asyncio.sleep(.1)
+                # assert handler 的被调此书为2
+                self.assertEqual(2, handler.async_handler.call_count)
+
+
+class TestPublicInfoService(IsolatedAsyncioTestCase):
+    async def test_get_info(self):
+        """获取公共信息"""
+        with patch.object(TeachingCalendarGetter, 'async_handler', return_value="Mocked Data"):
+            with patch.object(SessionValidator, 'async_handler', return_value=True):
+                handler = TeachingCalendarGetter()
+                service = PublicInfoService(name='calendar', handler=handler, update_expire=timedelta(days=1),
+                                            submit_expire=timedelta(seconds=10),
+                                            account_repository=session_repository)
+                result = await service.get_info('calendar')
+                self.assertIsNone(result)
+                # 如果数据没有更新，需要等待
+                if not await service.get_info('calendar'):
+                    await asyncio.sleep(.1)
+                result = await service.get_info('calendar')
+                self.assertEqual('Mocked Data', result)
+                # assert handler 的被调此书为1
+                self.assertEqual(1, handler.async_handler.call_count)
+
+    async def test_get_info_with_expired(self):
+        """测试获取公共信息，但是老数据过期"""
+        with patch.object(TeachingCalendarGetter, 'async_handler', return_value="Mocked Data"):
+            with patch.object(SessionValidator, 'async_handler', return_value=True):
+                handler = TeachingCalendarGetter()
+                service = PublicInfoService(name='calendar', handler=handler, update_expire=timedelta(microseconds=1),
+                                            submit_expire=timedelta(microseconds=1),
+                                            account_repository=session_repository)
+                result = await service.get_info('calendar')
+                self.assertIsNone(result)
+                # 如果数据没有更新，需要等待
+                await asyncio.sleep(.1)
+                result = await service.get_info('calendar')
                 self.assertIsNotNone(result)
                 await asyncio.sleep(.1)
                 # assert handler 的被调此书为2
