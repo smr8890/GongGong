d9bcbc3105cba565f861ff820aeeb348f7d746a8
LeoTan2004 <3509836544@qq.com>
2024-12-18 16:41:04 +0800

    feat(ics): 添加ics组件
    - Calendar表示一个完整的日历
    - Event表示一个完整的事件
    - RepeatRule(RRULE)表示一个重复规则
    - Alarm表示一个提醒的规则
    - 所有Component组件都可以调用to_ical()方法获取这个组件在icalendar中的表现形式
    
    Signed-off-by: LeoTan2004 <3509836544@qq.com>

---
+++ tests/test_icalendar.py
@@ -0,0 +1,47 @@
+import datetime
+from unittest import TestCase
+
+from xtu_ems.util.icalendar import BaseCalendar, BaseRepeatRule, BaseEvent, BaseAlarm
+
+
+class TestBaseCalendar(TestCase):
+    def test_to_ical(self):
+        target = """\
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Sky31//Gong 3.0//CN
+BEGIN:VEVENT
+DTSTAMP:20240101T000000Z
+SUMMARY:Christmas Celebration
+DESCRIPTION:Celebrate Christmas all day.
+LOCATION:Home
+DTSTART:20241225T120000
+DTEND:20241225T123000
+CATEGORIES:Holiday
+RRULE:FREQ=WEEKLY;COUNT=21
+BEGIN:VALARM
+ACTION:DISPLAY
+DESCRIPTION:Alarm
+TRIGGER:-PT22H
+END:VALARM
+UID:0ca8519470fb2c684ae26a8f010183dd@gong.sky31.com
+END:VEVENT
+END:VCALENDAR\
+"""
+        alarm = BaseAlarm(trigger=datetime.timedelta(days=-1, hours=2), description="Alarm")
+        event = BaseEvent(
+            summary="Christmas Celebration",
+            description="Celebrate Christmas all day.",
+            location="Home",
+            start_time=datetime.datetime(2024, 12, 25, 12, 0, 0),
+            end_time=datetime.datetime(2024, 12, 25, 12, 30, 0),
+            category="Holiday",
+            rrule=BaseRepeatRule(freq="WEEKLY", count=21),
+            alarm=alarm,
+            dtstamp=datetime.datetime(2024, 1, 1, 0, 0, 0)
+        )
+        calendar = BaseCalendar()
+        calendar.add_event(event)
+        ical_content = calendar.to_ical()
+        self.assertEqual(target, ical_content)
+        print(ical_content)
