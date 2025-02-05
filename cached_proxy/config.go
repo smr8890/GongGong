5f909a2005061ad23fe77db658f09d635f9ab184
smr8890 <2152918099@qq.com>
2025-02-05 18:43:01 +0800

    更新默认提醒时间并注释掉反馈服务配置

--- cached_proxy/config.go
+++ cached_proxy/config.go
@@ -15,13 +15,11 @@ var (
 var (
 	// DefaultCourseAlarms 课程事件的默认提醒
 	DefaultCourseAlarms = []icalendar.Alarm{
-		icalendar.NewIcsAlarm("DISPLAY", 28*time.Minute, "距离上课仅剩28分钟"),
+		icalendar.NewIcsAlarm("DISPLAY", 30*time.Minute, "距离上课仅剩30分钟"),
 	}
 	// DefaultExamAlarms 考试事件的默认提醒
 	DefaultExamAlarms = []icalendar.Alarm{
-		icalendar.NewIcsAlarm("DISPLAY", 1*time.Hour, "距离考试仅剩1小时"),
-		icalendar.NewIcsAlarm("DISPLAY", 24*time.Hour, "距离考试仅剩1天"),
-		icalendar.NewIcsAlarm("DISPLAY", 7*24*time.Hour, "距离考试仅剩7天"),
+		icalendar.NewIcsAlarm("DISPLAY", 30*time.Minute, "距离考试仅剩30分钟")
 	}
 )
 
